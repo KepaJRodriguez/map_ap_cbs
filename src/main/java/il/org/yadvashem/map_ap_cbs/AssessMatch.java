21cb6b55089ae562ed16eccc8e82a4fe28bc6b96
KepaJRodriguez <kepa.rodriguez@gmail.com>
2017-12-25 14:50:50 +0100

    New class: decided whether a match will be proposed to user

---
+++ src/main/java/il/org/yadvashem/map_ap_cbs/AssessMatch.java
@@ -0,0 +1,19 @@
+package il.org.yadvashem.map_ap_cbs;
+
+
+
+public class AssessMatch {
+
+	public static boolean assessMachNameString(String ap, CorporateBody cb){
+		boolean isMatch = false;
+	
+		double stringSimilarity = ComputeStringSimilarity.computeStringSimilarity(ap, cb);
+		if (stringSimilarity >= .95) {
+			isMatch = true;
+		}
+		
+		return isMatch;
+	}
+	
+	
+}
