49f714f078b9bd8a63664aa3c5f9122e074cf9ae
KepaJRodriguez <kepa.rodriguez@gmail.com>
2017-12-25 13:33:40 +0100

    First commit

---
+++ src/main/java/il/org/yadvashem/map_ap_cbs/App.java
@@ -0,0 +1,16 @@
+package il.org.yadvashem.map_ap_cbs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		String mode = args[0];
+		String ehriTable = args[1];
+		
+		
+    }
+}
