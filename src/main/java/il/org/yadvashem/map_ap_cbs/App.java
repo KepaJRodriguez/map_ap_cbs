825796f18a2a9f1d20a99f5237d804b4fd0d999b
KepaJRodriguez <kepa.rodriguez@gmail.com>
2017-12-27 10:15:11 +0100

    assessment updated

--- src/main/java/il/org/yadvashem/map_ap_cbs/App.java
+++ src/main/java/il/org/yadvashem/map_ap_cbs/App.java
@@ -1,18 +1,24 @@
 package il.org.yadvashem.map_ap_cbs;
 
+import java.io.IOException;
+import java.util.List;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
 		String ehriTable = args[0];
 		String apTable = args[1];
 		
+		List<CorporateBody> ehriPersonalities = ParseCBTable.parseCBsTable(ehriTable);
+		List<String> accessPoints = ParseAccessPointsTable.readAccessPointsTable(apTable);
+		ListMatches.listMatches(accessPoints, ehriPersonalities);
 		
-		
-		
+				
     }
 }
